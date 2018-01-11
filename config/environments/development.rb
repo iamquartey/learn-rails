4adbfd8223adc351df581468f6d15c6698fbb43d
Adnan Quartey <iamquartey@gmail.com>
2018-01-11 16:27:13 -0600

    sending mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_mailer_smtp_settings = {
+  config.action_mailer.smtp_settings = {
     address: "smtp.sendgrid.net",
     port: 587,
     domain: Rails.application.secrets.domain_name,
